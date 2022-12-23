f5cb197bb8519f85daf14c48594fe3d28e0cf0b3
teacher <tibedox@mail.ru>
2022-12-23 17:39:22 +0600

    Изменения

--- core/src/ru/myitschool/satgdx/SatGDX.java
+++ core/src/ru/myitschool/satgdx/SatGDX.java
@@ -69,7 +69,7 @@ public class SatGDX extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		// касания экрана
+		// касания экрана и клики мыши
 		if(Gdx.input.justTouched()){
 			touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touch);
